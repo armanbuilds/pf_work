bbceaeaafa8ad47985d09f9b2aec0cdd66b381de
Arman Adeel <aarmaan.09adeel@gmail.com>
2026-03-17 21:22:58 +0500

    week5 tasks

--- challenge3.cpp
+++ challenge3.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-main(){
-int t1 , t2;
-cout<<" temperture city 1 : ";
-cin>>t1;
-cout<<" temperature city 2 : ";
-cin>>t2;
-if(t1-t2 > 10){
-    cout<<" The difference is too big "<<endl;
-    cout<<" program ends"; 
-}
-else{ cout<<" program ends ";}
+int main() {
+    int n;
+    int hcf;
+    int lcm;
+    cout << "Enter the first number: ";
+    cin >> n;
+    cout << "Enter the second number: ";
+    cin >> hcf;
+    int a = n;
+    int b = hcf;
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    hcf = a;
+    lcm = (n * hcf) / hcf;
+    cout << "HCF: " << hcf << endl;
+    cout << "LCM: " << lcm << endl;
+    return 0;
 }
--- challenge4.cpp
+++ challenge4.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-main(){
-    int no1 , no2 ,no3  ;
-float price1 , price2 , price3 ;
-cout<< " Red roses : ";
-cin>>no1;
-cout<<" White roses : ";
-cin>>no2;
-cout<<" tulip : ";
-cin>>no3;
-price1 = 2.0;
-price2 = 4.10;
-price3 = 2.50;
-float totalprice = (price1 * no1) + (price2 * no2) + (price3 * no3);
-cout<<" origional price : "<<totalprice<<endl;
-if(totalprice > 200){
-    cout<<"dicounted price : "<<totalprice - (totalprice *  20/100); 
+int main() {
+    int n;
+    int rows;
+    int columns;
+    for(rows = 1; rows <= 5; rows++) {
+        for(columns = 1; columns <= rows; columns++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(rows = 5; rows >= 1; rows--) {
+        for(columns = 1; columns <= rows; columns++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
-else{ cout<<" dicounted price : 0 ";}
-}
+    
+    
